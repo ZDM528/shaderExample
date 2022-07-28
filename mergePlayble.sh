f57aaba1d20cc093732b8f1e725a746b2f40e629
hubluesky <hubluesky@126.com>
2022-07-28 17:03:53 +0800

    1、增加mergePlayble合并子模块提示功能

--- mergePlayble.sh
+++ mergePlayble.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # 用来删除合并子模块playable到当前项目中
+while true; do
+    read -p "这个命令将会合并playable子模块到当前项目中，此操作不可逆，请输入yes继续执行：" yn
+    case $yn in
+        [Yy]es ) break;;
+        [Nn]o ) exit;;
+        * ) echo "请输入 yes 或者 no.";;
+    esac
+done
 submodule_path=assets/scripts/playable &&\
 git rm --cached $submodule_path &&\
 git config -f .gitmodules --remove-section submodule.$submodule_path &&\
